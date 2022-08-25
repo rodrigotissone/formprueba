19314be22fb85b61a13277f217157411477b1078
Your Name <you@example.com>
2022-08-25 20:36:04 -0300

    first commit

---
+++ formulario.js
@@ -0,0 +1,10 @@
+
+    const nombre = document.querySelector("#inputNombre");
+    const apellido = document.querySelector("#inputapellido");
+    const email = document.querySelector("#inputemail");
+    const cuatro = document.querySelector("#cuatro");
+    const form = document.querySelector ("#form");  
+
+
+
+form.addEventListener ("submit") ;
